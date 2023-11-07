c68e8f572d9ae8679724a7c8879140e6a64e1cab
aneelabanda <aneela2106@gmail.com>
2023-11-07 11:38:18 -0600

    health

--- health.php
+++ health.php
@@ -57,7 +57,7 @@ $dbName = "CO2_emissions";
         // Store the Category ID in a "id" variable      
         // Creating an insert query using SQL syntax and
         // storing it in a variable.
-        $query = "SELECT * FROM `merged_data` WHERE Year = $year";
+        $query = "SELECT Country, CO2_emissions, $healthissue FROM `merged_data` WHERE Year = $year";
         $result1 = mysqli_query($con, $query);
         
         if (!$result1) {
@@ -127,7 +127,7 @@ function showUser(str) {
           <?php
           // Loop through the data and format it as JavaScript array elements
           while ($row = mysqli_fetch_assoc($result1)) {
-              echo "['" . $row['Country'] . "', " . $row['CO2_emissions'] . ", " . $row[``] . "],";
+              echo "['" . $row['Country'] . "', " . $row['CO2_emissions'] . ", " . $row["Iron deficiency"] . "";
           }
           ?>
         ]);
@@ -139,75 +139,7 @@ function showUser(str) {
         chart.draw(data, options);
       }
     </script>
-    <!-- <script type="text/javascript">
-      google.charts.load('current', {
-        'packages':['geochart'],
-      });
-      google.charts.setOnLoadCallback(drawRegionsMap);
- 
-      function drawRegionsMap() {
-        var data = google.visualization.arrayToDataTable([
-          ['Country', 'CO2_emissions'],
- 
-        // ?php
-        //  $query = "SELECT * FROM dropped_data";
- 
-        //  //storing the result of the executed query
-        //  $result = $conn->query($query);
-         
-        //  //initialize the array to store the processed data
-        //  $jsonArray = array();
-         
-        //  //check if there is any data returned by the SQL Query
-        //  if ($result->num_rows > 0) {
-        //    //Converting the results into an associative array
-        //    while($row = $result->fetch_assoc()) {
-        //      $jsonArrayItem = array();
-        //      $jsonArrayItem['Country'] = $row['Country'];
-        //      $jsonArrayItem['CO2_emissions'] = $row['CO2_emissions'];
-        //      //$jsonArrayItem['Year'] = $row['Year'];
-         
-        //      //append the above created object into the main array.
-        //      array_push($jsonArray, $jsonArrayItem);
-        //    }
-        //  }
-         
-        //  //Closing the connection to DB
-        //  $conn->close();
-
-         //if(mysqli_num_rows($result)> 0){
-
-          
-          //while($row = mysqli_fetch_array($result)){
-
-            //  echo "['".$row['Country']."', '".$row['CO2_emissions']."'],";
-
-          //}
-        //}
-
-
-      
-
-         
-        //  //set the response content type as JSON
-        //  header('Content-type: application/json');
-        //  //output the return value of json encode using the echo function.
-        //  echo json_encode($jsonArray);
-        //  $sql = "SELECT * FROM `dropped_data`";
-        //  $fire = mysqli_query($con,$sql);
-        //   while ($result = mysqli_fetch_assoc($fire)) {
-        //     echo"['".$result['Country']."',".$result['CO2_emissions']."],";
-        //   }
- 
-         //?>
-        ]);
-        var options = {};
- 
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
- 
-        chart.draw(data, options);
-      }
-    </script> -->
+    
 </head>
 <body>
     <header id="header"><div class="inner">
