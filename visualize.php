9f2cb98bc882affa7da0a293e16759a9df195079
aneelabanda <aneela2106@gmail.com>
2023-11-10 21:46:11 -0600

    visualize

--- visualize.php
+++ visualize.php
@@ -23,6 +23,8 @@ $dbName = "CO2_emissions";
    $con = mysqli_connect($servername, $username, $password, $dbName);
    $query = "SELECT * FROM `dropped_data`";
    $result = mysqli_query($con, $query);
+   $query_year = "SELECT DISTINCT Year FROM `merged_data`";
+   $result4 = mysqli_query($con, $query_year);
   //  if($con){
   //    echo "connected";
   //  }
@@ -203,38 +205,17 @@ function showUser(str) {
       <div class="flex-container">
   <select name="Year" id="Year" placeholder="Choose a Year">
   <option value="" disabled selected>Choose a Year to visualize the CO2 emissions</option>
-  <option value="1990">1990</option>
-  <option value="1991">1991</option>
-  <option value="1992">1992</option>
-  <option value="1993">1993</option>
-  <option value="1994">1994</option>
-  <option value="1995">1995</option>
-  <option value="1996">1996</option>
-  <option value="1997">1997</option>
-  <option value="1998">1998</option>
-  <option value="1999">1999</option>
-  <option value="2000">2000</option>
-  <option value="2001">2001</option>
-  <option value="2002">2002</option>
-  <option value="2003">2003</option>
-  <option value="2004">2004</option>
-  <option value="2005">2005</option>
-  <option value="2006">2006</option>
-  <option value="2007">2007</option>
-  <option value="2008">2008</option>
-  <option value="2009">2009</option>
-  <option value="2010">2010</option>
-  <option value="2011">2011</option>
-  <option value="2012">2012</option>
-  <option value="2013">2013</option>
-  <option value="2014">2014</option>
-  <option value="2015">2015</option>
-  <option value="2016">2016</option>
-  <option value="2017">2017</option>
-  <option value="2018">2018</option>
-  <option value="2019">2019</option>
-  <option value="2020">2020</option>
-  <option value="2021">2021</option>
+  <?php
+    while ($row = mysqli_fetch_assoc($result4)){
+    ?>
+    <option value="<?php echo $row['Year']; ?>">
+
+<?php echo $row['Year']; ?>
+
+</option>
+ <?php
+    }
+    ?>
   </select>
   <input type="submit" value="Submit" name="submit"></div>
 </form>
