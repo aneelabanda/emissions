5f7e0fcae2e916581cdc6041a3a7c4da71c02464
aneelabanda <aneela2106@gmail.com>
2023-12-05 21:22:01 -0600

    as

--- health.php
+++ health.php
@@ -271,7 +271,7 @@ function showPage() {
   <option value="1992">1992</option>
   <option value="1993">1993</option> -->
   </select>
-  <input type="submit" value="Submit" name="submit" onsubmit="showPage()"></div>
+  <input type="submit" value="Submit" name="submit" onclick="showPage()"></div>
 </form>
     </div>
   </section>
